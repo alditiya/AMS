f27de31e4ef3c1b1b4d08b6be809767b95727d17
Alditiya Yudha Pratama <aldit4@gmail.com>
2023-10-30 11:27:52 +0700

    change details to Icon

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -58,8 +58,14 @@ const Table = ({ todos, setTodos, isLoading }) => {
                       </span>
                     </td>
                     <td className='p-3'>{new Date(todoItem.created).toLocaleString()}</td>
-                    <td className='p-3 font-medium grid grid-flow-col items-center mt-3 ml-5'>
-                      <button className="btn btn-ghost btn-xs">details</button>
+                    <td className='p-3 font-medium grid grid-flow-col items-center'>
+                      <button className="btn btn-ghost btn-xs">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                        <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                        <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
+                      </svg>
+                      </button>
+                      
                     </td>
                   </tr>
                 )
